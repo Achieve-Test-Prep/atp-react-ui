d08540354a0973bfb8b7f0fe5a5da6cd6c660cd6
Adeel Ali <80695471+adeel-ali-atp@users.noreply.github.com>
2023-02-22 15:04:13 -0500

    test(alert): add test
    tests added for alert component

--- src/Alert/alert-icons.tsx
+++ src/Alert/alert-icons.tsx
@@ -9,6 +9,8 @@ export const InfoIcon: React.FC<React.PropsWithChildren<AlertIconProps>> = (prop
     strokeWidth="2"
     viewBox="0 0 24 24"
     stroke="currentColor"
+    aria-hidden="true"
+    aria-label="info"
   >
     <path d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
   </svg>
@@ -23,6 +25,8 @@ export const WarningIcon: React.FC<React.PropsWithChildren<AlertIconProps>> = (p
     strokeWidth="2"
     viewBox="0 0 24 24"
     stroke="currentColor"
+    aria-hidden="true"
+    aria-label="warning"
   >
     <path d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
   </svg>
@@ -37,6 +41,8 @@ export const DangerIcon: React.FC<React.PropsWithChildren<AlertIconProps>> = (pr
     strokeWidth="2"
     viewBox="0 0 24 24"
     stroke="currentColor"
+    aria-hidden="true"
+    aria-label="danger"
   >
     <path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
   </svg>
@@ -51,6 +57,8 @@ export const SuccessIcon: React.FC<React.PropsWithChildren<AlertIconProps>> = (p
     strokeWidth="2"
     viewBox="0 0 24 24"
     stroke="currentColor"
+    aria-hidden="true"
+    aria-label="success"
   >
     <path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
   </svg>
@@ -65,6 +73,8 @@ export const NeutralIcon: React.FC<React.PropsWithChildren<AlertIconProps>> = (p
     strokeWidth="2"
     viewBox="0 0 24 24"
     stroke="currentColor"
+    aria-hidden="true"
+    aria-label="neutral"
   >
     <path d="M8 12h.01M12 12h.01M16 12h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
   </svg>
--- src/__tests__/alert.test.tsx
+++ src/__tests__/alert.test.tsx
@@ -1,76 +1,68 @@
 import React from 'react';
-
-import { mount } from 'enzyme';
+import { render, screen, fireEvent } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event'
 
 import { Alert } from '../Alert';
 import { SuccessIcon, DangerIcon, WarningIcon, InfoIcon, NeutralIcon } from '../Alert/alert-icons';
-
+const test = (
+  <svg
+    className="h-5 w-5 text-success tb:text-green-300 absolute left-0 top-0 ml-4 mt-4"
+    fill="none"
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    viewBox="0 0 24 24"
+  >
+    <path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+  </svg>
+);
 describe('Avatar', () => {
   it('should render without crashing', () => {
-    mount(<Alert />);
+    render(<Alert />);
   });
 
   it('should not contain a close button', () => {
-    const wrapper = mount(<Alert />);
-
-    expect(wrapper.find('button')).toHaveLength(0);
+    render(<Alert />);
+    expect(screen.queryByRole('button')).toEqual(null);
   });
 
   it('should contain a close button', () => {
-    const wrapper = mount(<Alert onClose={() => {}} />);
-
-    expect(wrapper.find('button')).toHaveLength(1);
+    render(<Alert onClose={() => {}} />);
+    expect(screen.getByLabelText('close')).not.toEqual(null);
   });
 
   it('should call a function when close button is clicked', () => {
     const onClose = jest.fn();
-    const wrapper = mount(<Alert onClose={onClose} />);
+    render(<Alert onClose={onClose} />);
+    const closeBtn = screen.getByLabelText('close');
 
-    wrapper.find('button[aria-label="close"]').simulate('click');
-
+    fireEvent.click(closeBtn);
     expect(onClose).toHaveBeenCalled();
   });
 
   it('should render a success icon', () => {
-    const wrapper = mount(<Alert type="success" />);
-    const Icon = SuccessIcon;
-
-    expect(wrapper.find(Icon)).toHaveLength(1);
+    const { container, getByLabelText } = render(<Alert type="success" />);
+    expect(getByLabelText('success')).toBeTruthy();
   });
 
   it('should render a error icon', () => {
-    const wrapper = mount(<Alert type="error" />);
-    const Icon = DangerIcon;
-
-    expect(wrapper.find(Icon)).toHaveLength(1);
+    const { container, getByLabelText } = render(<Alert type="error" />);
+    expect(getByLabelText('danger')).toBeTruthy();
   });
 
   it('should render an info icon', () => {
-    const wrapper = mount(<Alert type="info" />);
-    const Icon = InfoIcon;
-
-    expect(wrapper.find(Icon)).toHaveLength(1);
+    const { container, getByLabelText } = render(<Alert type="info" />);
+    expect(getByLabelText('info')).toBeTruthy();
   });
 
   it('should render a warning icon', () => {
-    const wrapper = mount(<Alert type="warning" />);
-    const Icon = WarningIcon;
-
-    expect(wrapper.find(Icon)).toHaveLength(1);
+    const { container, getByLabelText } = render(<Alert type="warning" />);
+    expect(getByLabelText('warning')).toBeTruthy();
   });
 
   it('should render a neutral icon', () => {
-    const wrapper = mount(<Alert />);
-    const Icon = NeutralIcon;
-
-    expect(wrapper.find(Icon)).toHaveLength(1);
-  });
-
-  it('should render a neutral icon for an invalid type', () => {
-    // @ts-expect-error AA - old-compatibility
-    const wrapper = mount(<Alert type="invalid" />);
-    const Icon = NeutralIcon;
-
-    expect(wrapper.find(Icon)).toHaveLength(1);
+    const { container, getByLabelText } = render(<Alert type="neutral" />);
+    expect(getByLabelText('neutral')).toBeTruthy();
   });
 });
