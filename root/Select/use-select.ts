32188aa5efcb29071f2548a81575c5e8f535649a
Adeel Ali <adeel.ali@achieve-test-prep.com>
2023-07-27 14:39:48 -0400

    chore(update dependencies): dependency update
    typescript 5

--- root/Select/use-select.ts
+++ root/Select/use-select.ts
@@ -1,24 +1,17 @@
-import { ChangeEvent /* , useContext */, useCallback } from 'react';
+import { ChangeEvent /* , useContext */, useCallback, useContext } from 'react';
 
 import { twMerge } from 'tailwind-merge';
 
-import type { ISelectOption, SelectProps } from './types';
+import { ThemeContext } from '../themes/theme-context';
 
-// Select
-const select = {
-  base: 'block w-full p-2.5 text-sm text-accent-light focus:outline-none tb:text-gray-300 rounded-lg',
-  active:
-    'focus:border-primary border-outline tb:border-outline tb:bg-gray-700 focus:ring focus:ring-primary-light tb:focus:ring-gray-300 tb:focus:border-outline',
-  select: 'leading-5',
-  disabled: 'disabled:cursor-not-allowed disabled:text-accent-disabled disabled:bg-gray-50 disabled:border-outline',
-};
+import type { ISelectOption, SelectProps } from './types';
 
 export default function useSelect(props: SelectProps) {
   const { onChangeSelection, options, className, multiple, disabled, children, ...other } = props;
 
-  // const {
-  //   theme: { select },
-  // } = useContext(ThemeContext);
+  const {
+    theme: { select },
+  } = useContext(ThemeContext);
 
   const styleCls = twMerge(
     select.base,
