d42847a7004fb679346d76901525f495ed229a19
KhubaibQaiser <khubaib.qaiser@achieve-test-prep.com>
2023-08-02 07:36:31 -0400

    Add documentation

--- src/utils/tw-sort.ts
+++ src/utils/tw-sort.ts
@@ -1,3 +1,11 @@
+/**
+ * A function to concatenate a tagged template literal and its substitutions.
+ *
+ * @param {TemplateStringsArray} strings - The raw strings from the template literal.
+ * @param {...any} values - The substitution values in the template literal.
+ * @returns {string} The resulting string, with substitutions inserted.
+ */
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function twSort(strings: TemplateStringsArray, ...values: any[]): string {
   let result = '';
