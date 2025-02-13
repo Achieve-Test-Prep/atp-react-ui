53cf71044aa1eb300d09feb4d5199f2ebe27cd8a
Adeel Ali <adeel.ali@achieve-test-prep.com>
2025-02-13 06:12:42 -0500

    fix: story book

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,19 +2,26 @@ import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
   stories: ['../src/__stories__/**/*.mdx', '../src/__stories__/**/*.stories.@(js|jsx|ts|tsx)'],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-styling',
+    '@storybook/addon-webpack5-compiler-babel',
+    '@chromatic-com/storybook'
   ],
+
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
   },
-  docs: {
-    autodocs: 'tag',
-  },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 };
 
 export default config;
