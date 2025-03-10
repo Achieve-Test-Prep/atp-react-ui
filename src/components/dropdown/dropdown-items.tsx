e390923d90ed6c2d6ac8706d012eb1b71b2f193c
Adeel Ali <adeel.ali@achieve-test-prep.com>
2025-03-10 10:12:58 -0400

    fix: animation

--- src/components/dropdown/dropdown-items.tsx
+++ src/components/dropdown/dropdown-items.tsx
@@ -15,5 +15,16 @@ export const DropdownItems: React.ForwardRefExoticComponent<
   const { dropdown } = useTheme();
   const cls = twMerge(dropdown.base, dropdown.align[align], className);
 
-  return <MenuItems transition as={as} ref={ref} {...rest} className={cls} />;
+  return (
+    <MenuItems
+      transition
+      as={as}
+      ref={ref}
+      {...rest}
+      className={twMerge(
+        'origin-top transition duration-200 ease-out data-[closed]:scale-95 data-[closed]:opacity-0',
+        cls
+      )}
+    />
+  );
 });
