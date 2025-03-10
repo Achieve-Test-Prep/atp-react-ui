2d68ebf2716a7c271d38b37ed7336432ac7474b2
Adeel Ali <adeel.ali@achieve-test-prep.com>
2025-03-10 07:21:10 -0400

    build(move code and build system to vite): move code
    and build system to vite
    
    BREAKING CHANGE: removed headless ui export from package

--- src/components/form-components/index.ts
+++ src/components/form-components/index.ts
@@ -4,3 +4,4 @@ export * from './input';
 export * from './radio';
 export * from './select';
 export * from './textarea';
+export * from './types';
--- vite.config.ts
+++ vite.config.ts
@@ -8,30 +8,17 @@ export default defineConfig({
   plugins: [
     react(),
     dts({
-      include: [
-        'src/components',
-        'src/hocs',
-        'src/hooks',
-        'src/utils',
-        'src/themes',
-        'src/atp-lib.tsx',
-        'src/types.ts',
-      ],
-      exclude: [
-        '**/*.stories.tsx',
-        '**/*.stories.ts',
-        '**/*.test.tsx',
-        '**/*.test.ts',
-      ],
       insertTypesEntry: true,
+      include: ['src/**/*.ts', 'src/**/*.tsx'],
+      outDir: 'dist',
     }),
   ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'WikzUI',
-      formats: ['es'], // Only include ES modules format
-      fileName: () => `index.js`,
+      name: 'AtpReactUI',
+      formats: ['es'],
+      fileName: 'index',
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
@@ -40,15 +27,7 @@ export default defineConfig({
           react: 'React',
           'react-dom': 'ReactDOM',
         },
-        assetFileNames: assetInfo => {
-          if (assetInfo.name === 'style.css') return 'index.css';
-          return assetInfo.name;
-        },
       },
     },
-
-    // Ensure CSS is processed
-    cssCodeSplit: false,
-    assetsInlineLimit: 0, // Add this line to ensure Tailwind styles are properly included
   },
 });
